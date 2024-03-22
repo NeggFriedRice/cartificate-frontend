96eb6452f5d55b679b7c12680ab016aff5fd0ebc
Thomas Loo <tom@TomsPC>
2024-03-22 14:38:56 +1100

    Fix bug where user is logged out on refresh. Add async await to getUpdates and filterUpdates to display correct entries upon refresh

--- src/components/ShowUpdate.jsx
+++ src/components/ShowUpdate.jsx
@@ -1,38 +1,38 @@
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
-const ShowUpdate = ({ updates, user }) => {
+const ShowUpdate = ({ updates, filtered, user }) => {
 
-  const [filtered, setFiltered] = useState(null)
+  // const [filtered, setFiltered] = useState(null)
 
-  const userObject = JSON.parse(user)
+  // const userObject = JSON.parse(user)
 
   function dateMod(date) {
     return date.split('T')[0]
   }
 
-  function filterByUser(update) {
-    if (update.createdBy == userObject._id) {
-      return true
-    }
-  }
+  // function filterByUser(update) {
+  //   if (update.createdBy == userObject._id) {
+  //     return true
+  //   }
+  // }
 
-  function filterUpdates() {
-    try {
-      const filteredUpdates = updates.filter(filterByUser)
-      setFiltered(filteredUpdates)
-    } catch (err) {
-      setFiltered(false)
-    }
-  }
+  // function filterUpdates() {
+  //   try {
+  //     const filteredUpdates = updates.filter(filterByUser)
+  //     setFiltered(filteredUpdates)
+  //   } catch (err) {
+  //     setFiltered(false)
+  //   }
+  // }
 
-  useEffect(() => {
-    filterUpdates()
-  }, [user])
+  // useEffect(() => {
+  //   filterUpdates()
+  // }, [user])
 
   return (
     <>
-    <div className="log-container">
+    <div className="w-[300px] text-md p-4">
       <ul>
         {!filtered || filtered.length == 0 ? 
         <div className="flex justify-center py-8">
@@ -40,10 +40,10 @@ const ShowUpdate = ({ updates, user }) => {
         </div> 
         :
         filtered.map((update, index) => 
-          <li key={index}>
+          <li key={index} className="py-2">
               <Link to={`updates/${update._id}`}>
-                <div className="update-homepage">
-                  <h3 className="update-link">{index + 1}. {update.activity}</h3>
+                <div className="flex justify-between">
+                  <h3 className="text-lg">{index + 1}. {update.activity}</h3>
                   <h3>${update.cost}</h3>
                 </div>
               </Link>
--- src/components/UpdateForm.jsx
+++ src/components/UpdateForm.jsx
@@ -43,49 +43,48 @@ const UpdateForm = ({updates, addUpdate}) => {
     console.log(activity)
   }
 
+  const inputFormat = 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-full focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+  const headingFormat = 'block mb-2 text-sm font-medium text-white text-[1.25rem]'
 
   return (
     <>
-      <form onSubmit={submitHandler}>
-        <h1>Maintenance logger</h1>
-        <h2>Activity type</h2>
-        <input className="input is-rounded" type="text" name="activity" placeholder="Oil change, replaced brake rotors" value={activity.name} onChange={changeHandler}/>
-        {/* <h2>Date</h2>
-        <DatePicker selected={date} onChange={(date) => setDate(date)} dateFormat="dd/MM/yy"/> */}
-              <div className='m-4'>
-                <label className='block mb-2 text-sm font-medium text-gray-900'>Date</label>
-                <input
-                    type='date'
-                    name='date'
-                    id='date'
-                    pattern='\d{4}-\d{2}-\d{2}'
-                    value={updates.date}
-                    onChange={changeHandler}
-                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-full focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
-                    placeholder=''
-                    required=''
-                />
-            </div>
-        <h2>Cost</h2>
-        <input className="input is-rounded" type="tel" name="cost" placeholder="$" value={activity.cost} onChange={changeHandler}/>
-        <h2>Receipt</h2>
-        <div className="file">
-          <label className="file-label">
-            <input className="file-input" type="file" name="resume" />
-            <span className="file-cta">
-              <span className="file-icon">
-                <i className="fas fa-upload"></i>
-              </span>
-              <span className="file-label">
-                Choose a file…
-              </span>
-            </span>
-          </label>
+    <div className="flex justify-center">
+      <form onSubmit={submitHandler} className="w-[100%] px-8">
+        <div className="my-4">
+          <h2 className={headingFormat}>Activity type</h2>
+          <input className={inputFormat} type="text" name="activity" placeholder="Oil change, replaced brake rotors" value={activity.name} onChange={changeHandler}/>
         </div>
-        <h2>Extra notes</h2>
-        <textarea className="textarea" rows="10" name="notes" value={activity.notes} onChange={changeHandler}></textarea>
-        <button className="button is-primary is-light" type="submit">Submit</button>        
+        <div className="my-4">
+          <label className={headingFormat}>Date</label>
+          <input
+              type='date'
+              name='date'
+              id='date'
+              pattern='\d{4}-\d{2}-\d{2}'
+              value={updates.date}
+              onChange={changeHandler}
+              className={inputFormat}
+              placeholder=''
+              required=''
+          />
+      </div>
+      <div className="my-4">
+        <h2 className='block mb-2 text-sm font-medium text-white text-[1.25rem]'>Cost</h2>
+        <input className={inputFormat} type="tel" name="cost" placeholder="$" value={activity.cost} onChange={changeHandler}/>
+      </div>
+        <h2 className={headingFormat}>Receipt</h2>
+        <div className="file my-4">
+          <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-grey-700 dark:placeholder-gray-400" id="file_input" type="file" />
+        </div>
+        <h2 className={headingFormat}>Extra notes</h2>
+        <div className="flex flex-col my-4">
+          <textarea className="textarea bg-white mb-4" rows="10" name="notes" value={activity.notes} onChange={changeHandler}></textarea>
+          <div className="flex justify-center">
+           <button className="bg-green-800 w-[50%] rounded-full py-2" type="submit">Submit</button>
+          </div>
+        </div> 
       </form>
+    </div>
       {/* <button className="button is-primary is-light" onClick={showUpdates}>Console Log updates</button> */}
     </>
   )
