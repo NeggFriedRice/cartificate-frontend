f6df64169b960f9a21e02ae79c77dae39f52aef9
Thomas Loo <tom@TomsPC>
2024-03-22 17:08:46 +1100

    Add new colour palette set to tailwind.config. Change some colour styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'block-xs': '0.25rem 0.25rem black',
+        'block-sm': '0.5rem 0.5rem black',
+        'block-smmd': '0.75rem 0.75rem black',
+        'block-md': '1rem 1rem',
+        'block-lg': '1.5rem 1.5rem black',
+      },
+  },
+  colors: {
+    setPurpleDark: '#583D72',
+    setPurpleLight: '#9F5F80',
+    setCoral: '#FF8474',
+    setPeach: '#FFC996'
   },
   plugins: [],
-}
+}}
