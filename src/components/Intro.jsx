ef348abffae8e2506514a97add1ffae94f812b44
NeggFriedRice <tomloo222@gmail.com>
2024-03-25 20:29:06 +1100

    Change cursor SVG on intro message

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -31,8 +31,8 @@ export default function Intro(key) {
         <div className="animate-float transition-all flex justify-center py-8 mt-24 bg-setPeach hover:bg-setPeachLight rounded-full shadow-block-md hover:shadow-block-lg shadow-setPurpleDark hover:shadow-setPurpleDark">
             <p className="text-setPurpleDark lg:text-[1.5rem]">Get your maintenance started!</p>
             <span className="relative flex top-[1px] right-[12px] h-5 w-5 ">
-            <img className="absolute w-[20px] lg:w-[25px] mx-4 animate-ping duration-1000 opacity-75" src="pointer.svg" />
-            <img className="relative inline-flex w-[20px] lg:w-[25px] mx-4 " src="pointer.svg" />
+            <img className="absolute w-[35px] lg:w-[35px] mx-4 animate-ping duration-1000 opacity-75" src="cursor.svg" />
+            <img className="relative inline-flex w-[35px] lg:w-[35px]  mx-4 " src="cursor.svg" />
             </span>
         </div>
     </Link>
