83f474b3d5816ef8b6a0b802e7633629732762b9
NeggFriedRice <tomloo222@gmail.com>
2024-03-28 19:38:25 +1100

    Change styling and position of sign out and edit vehicle details buttons in profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -48,11 +48,15 @@ export default function Profile({ user, setUser, setIsLoggedIn, getUpdates }) {
 		exit={animationSlide.exit}
 		className="my-8 p-4">
 			<div className="">
-				<div className="bg-[#f5fda9] rounded-lg text-setPurpleDark p-4 text-[1.5rem] shadow-block-sm hover:shadow-block-smmd transition-all duration-700">
-				<h1 className=" font-bold">{user.username}</h1>
-				<button type="button" onClick={logOut} className="bg-blue-500 hover:bg-blue-400 text-white text-[1rem] transition-all duration-700 px-2 rounded-lg mt-4">Sign out</button>
+				<div className="bg-[#f5fda9] rounded-lg text-setPurpleDark p-4 text-[1.5rem] shadow-block-sm hover:shadow-block-smmd transition-all duration-700 flex justify-between items-center">
+				<div>
+					<h1 className=" font-bold">{user.username}</h1>
 				</div>
-			<div className="bg-[#fad19b] rounded-lg text-setPurpleDark p-4 my-4 text-[1.25rem] shadow-block-sm hover:shadow-block-smmd transition-all duration-700">
+				<div>
+					<button type="button" onClick={logOut} className="bg-blue-500 hover:bg-blue-400 text-white text-[1rem] transition-all duration-700 px-4 py-1 rounded-lg self-start">Sign out</button>
+				</div>
+				</div>
+			<div className="bg-[#fad19b] rounded-lg text-setPurpleDark p-4 my-4 text-[1.25rem] shadow-block-sm hover:shadow-block-smmd transition-all duration-700 flex justify-between">
 				<div className="">
 					<p><span className="font-bold">Brand:</span> {user.vehicle.brand}</p>
 					<p><span className="font-bold">Model:</span> {user.vehicle.model}</p>
@@ -60,9 +64,11 @@ export default function Profile({ user, setUser, setIsLoggedIn, getUpdates }) {
 					<p><span className="font-bold">Registration:</span> {user.vehicle.registration}</p>
 					<p><span className="font-bold">VIN:</span> {user.vehicle.vin}</p>
 				</div>
-				<button className="bg-teal-500 hover:bg-teal-400 text-white text-[1rem] transition-all duration-700 px-2 rounded-lg mt-4">
-					<Link to="/profile/update">Edit</Link>
-				</button>
+				<div>
+					<button className="bg-teal-500 hover:bg-teal-400 text-white text-[1rem] transition-all duration-700 px-4 py-1 rounded-lg">
+						<Link to="/profile/update">Edit</Link>
+					</button>
+				</div>
 			</div>
 			</div>
 		</motion.div>
