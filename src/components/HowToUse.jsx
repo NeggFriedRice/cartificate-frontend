3f3250298c2337d336ad7a492122b4633ecb3014
Thomas Loo <tomloo222@gmail.com>
2024-07-11 12:03:21 +1000

    Update welcome how-to message

--- src/components/HowToUse.jsx
+++ src/components/HowToUse.jsx
@@ -11,6 +11,7 @@ export default function HowToUse() {
     exit={animationSlide.exit}>
         <div className="flex justify-center animate-floatxs py-12 text-setPurpleDark text-[1.25rem] max-xs:text-[1rem]">
                 <div className="bg-setPeach w-[350px] rounded-r-[15px] rounded-bl-[15px] p-4 shadow-block-md hover:shadow-block-lg shadow-setPurpleDark hover:shadow-setPurpleDark transition-all duration-700 lg:w-[700px] lg:py-6">
+                    <p className="text-[1.5rem] font-bold py-4">Welcome to CARtificate, the easiest vehicle maintenance tracker you'll ever use!</p>
                 <ul className="list-disc px-8 text-setPurpleDark">
                     <li>
                         <Link to="/register"><p className="underline">Register an account</p></Link>
@@ -31,7 +32,7 @@ export default function HowToUse() {
                         <p>You also have the option of adding your vehicle details in your profile in the top right hand corner</p>
                     </li>
                 </ul>
-                <p className="py-8">You can go back and edit or delete any entries that you have submitted at any time, happy tracking ~</p>
+                <p className="py-8">Feel free to edit and delete entries as you see fit, happy tracking!</p>
                 </div>
         </div>
     </motion.div>
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,60 +1,78 @@
-import React from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { Link } from "react-router-dom";
 
 export default function Intro(key) {
-
   const containerVariants = {
     hidden: {
-      y: '-100%',
+      y: "-100%",
       opacity: 0,
     },
     visible: {
       y: 0,
       opacity: 1,
-      transition: { delay: 1.25, type: 'spring', stiffness: 200 }
+      transition: { delay: 1.25, type: "spring", stiffness: 200 },
     },
     exit: {
-      x: '-200%',
+      x: "-200%",
       opacity: 0,
-      transition: { duration: 0.1 }
-    }
-  }
+      transition: { duration: 0.1 },
+    },
+  };
 
   return (
     <>
-    <motion.div
-    variants={containerVariants}
-    initial="hidden"
-    animate="visible"
-    exit="exit"
-    whileTap={{ scale: 0.95 }}>
-    <Link to="/login">
-        <div className="animate-float transition-all duration-700 flex justify-center py-8 mt-24 bg-setPeach hover:bg-setPeachLight rounded-full shadow-block-md hover:shadow-block-lg shadow-setPurpleDark hover:shadow-setPurpleDark">
-            <p className="text-setPurpleDark lg:text-[1.5rem]">Get your maintenance started!</p>
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        whileTap={{ scale: 0.95 }}
+      >
+        <Link to="/login">
+          <div className="animate-float transition-all duration-700 flex justify-center py-8 mt-24 bg-setPeach hover:bg-setPeachLight rounded-full shadow-block-md hover:shadow-block-lg shadow-setPurpleDark hover:shadow-setPurpleDark">
+            <p className="text-setPurpleDark lg:text-[1.5rem]">
+              Get your maintenance started!
+            </p>
             <span className="relative flex top-[1px] right-[12px] h-5 w-5 ">
-            <img className="absolute w-[35px] lg:w-[35px] mx-4 animate-ping duration-1000 opacity-75" src="cursor.svg" />
-            <img className="relative inline-flex w-[35px] lg:w-[35px]  mx-4 " src="cursor.svg" />
+              <img
+                className="absolute w-[35px] lg:w-[35px] mx-4 animate-ping duration-1000 opacity-75"
+                src="cursor.svg"
+              />
+              <img
+                className="relative inline-flex w-[35px] lg:w-[35px]  mx-4 "
+                src="cursor.svg"
+              />
             </span>
-        </div>
-    </Link>
-    </motion.div>
-    <motion.div
-    variants={containerVariants}
-    initial="hidden"
-    animate={{ y: 0, opacity: 1, transition: {delay: 1.5, duration: 0.1, type: 'spring', stiffness: 200 }}}
-    exit="exit"
-    whileTap={{ scale: 0.95 }}>
-      <div className="animate-floatxs transition-all duration-700 flex justify-center py-4 mt-24 bg-[#D9EDBF] hover:bg-[#b9d98f] rounded-full shadow-block-sm hover:shadow-block-smmd shadow-setPurpleDark hover:shadow-setPurpleDark w-[250px]">
-      <Link to="/info">
-        <div className="w-64 flex justify-center">
-          <p className="text-setPurpleDark lg:text-[1.25rem] ">How to use</p>
-        </div>
-      </Link>
           </div>
-
-    </motion.div>
+        </Link>
+      </motion.div>
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        animate={{
+          y: 0,
+          opacity: 1,
+          transition: {
+            delay: 1.5,
+            duration: 0.1,
+            type: "spring",
+            stiffness: 200,
+          },
+        }}
+        exit="exit"
+        whileTap={{ scale: 0.95 }}
+      >
+        <Link to="/info">
+          <div className="animate-floatxs transition-all duration-700 flex justify-center py-4 mt-24 bg-[#D9EDBF] hover:bg-[#b9d98f] rounded-full shadow-block-sm hover:shadow-block-smmd shadow-setPurpleDark hover:shadow-setPurpleDark w-[250px]">
+            <div className="w-64 flex justify-center">
+              <p className="text-setPurpleDark lg:text-[1.25rem] ">
+                Welcome!
+              </p>
+            </div>
+          </div>
+        </Link>
+      </motion.div>
     </>
-  )
+  );
 }
